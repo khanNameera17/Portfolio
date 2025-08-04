7b1bceda1048b3f1d9278c36f4a2c5508fab0b06
Nameera khan <nameerakhan54@icloud.com>
2025-08-04 18:03:55 +0530

    horizontal scroll in about us page

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,16 +1,36 @@
-// components/About.jsx
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import "./style.scss";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    let sections = gsap.utils.toArray(".card");
+
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: scrollRef.current,
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        end: () => "+=" + scrollRef.current.offsetWidth,
+      },
+    });
+  }, []);
+
   return (
-    <div className="about-container" >
-      <h2>About White-Cinette</h2>
-      <p>
-        White-Cinette is a creative design studio specializing in bold,
-        immersive experiences through code, 3D, and animation. We turn ideas
-        into stunning digital realities.
-      </p>
+    <div className="scrolling-wrapper" ref={scrollRef}>
+      <div className="card"><h2>About Us</h2></div>
+      <div className="card"><h2>Our Vision</h2></div>
+      <div className="card"><h2>Team</h2></div>
+      <div className="card"><h2>Why Choose Us</h2></div>
     </div>
   );
 };
