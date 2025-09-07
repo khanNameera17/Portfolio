c9440d950920f3901cdba79ce6425d1c786f0ebc
Nameera khan <nameerakhan54@icloud.com>
2025-09-07 19:07:13 +0530

    service section ui changes

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -4,43 +4,63 @@ import "./style.scss";
 
 const services = [
   {
-    title: "SEO",
+    title: "Web Development",
     // bgColor: "#fde9d9", // light peach
     icon: "👁️", // you can replace with SVG/icon
   },
   {
-    title: "USER EXPERIENCE",
+    title: "App Development",
     // bgColor: "#a4f3d2", // mint green
     icon: "🧠",
   },
   {
-    title: "Web App",
+    title: "WordPress",
     // bgColor: "#d1d2ff", // soft purple
     icon: "💻",
   },
   {
-   title: "Mobile App",
+   title: "Shopify",
    // bgColor: "blue", // soft purple
    icon: "💻",
  },
- 
+ {
+  title: "Framer",
+  // bgColor: "blue", // soft purple
+  icon: "💻",
+},
 ];
 
 const Services = () => {
-  return (
-    <section className="services-section">
-      {services.map((service, index) => (
-        <div
-          key={index}
-          className="service-card"
-          style={{ backgroundColor: service.bgColor }}
-        >
-          <div className="service-icon">{service.icon}</div>
-          <h2 className="service-title">{service.title}</h2>
-        </div>
-      ))}
-    </section>
-  );
+ return (
+   <section className="services-section">
+     <div className="row">
+       {services.slice(0, 3).map((service, index) => (
+         <div
+           // key={index}
+           className="service-card"
+           style={{ backgroundColor: service.bgColor }}
+         >
+           {/* <div className="service-icon">{service.icon}</div> */}
+           <h2 className="service-title">{service.title}</h2>
+         </div>
+       ))}
+     </div>
+
+     <div className="row">
+       {services.slice(3).map((service, index) => (
+         <div
+       
+           className="service-card"
+           style={{ backgroundColor: service.bgColor }}
+         >
+           {/* <div className="service-icon">{service.icon}</div> */}
+           <h2 className="service-title">{service.title}</h2>
+         </div>
+       ))}
+     </div>
+   </section>
+ );
 };
+
 
 export default Services;
